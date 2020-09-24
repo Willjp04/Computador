9cdd0b88bff7d5eb9a863d79d905443ca25bc6f1
Willian Sostenes <sostenes655@gmail.com>
2020-09-24 20:13:18 -0300

    Inicio de Projeto pc

---
+++ Aula05/src/aula05/Computador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class Computador {
+    public String marca;
+    public String processador;
+    public String perifericos;
+    int memoria;
+    void status() {
+        System.out.println("Marca " + this.marca);
+    
+}
+    
+   
+    
+    
+}
